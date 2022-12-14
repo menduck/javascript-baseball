cb709bac81af4bcc4c34ace485cc31fd7ebecea4
ming0830 <minsun0830@naver.com>
2022-12-15 05:04:33 +0900

    feat : 스트라이크와 볼의 개수에 따라 출력하는 기능 구현

--- src/Constant.js
+++ src/Constant.js
@@ -1,10 +1,13 @@
+const SUCCESS = 3;
+
 const INPUT_MESSAGE = {
   inputThreeNumberMessage : "숫자를 입력해주세요 : ",
   optionNumberMessage : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
 }
 
 const OUTPUT_MESSAGE = {
-  gameGuideMessage : "숫자 야구 게임을 시작합니다."
+  gameStartGuideMessage : "숫자 야구 게임을 시작합니다.",
+  gameOverGuideMessage : "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
 
 }
 
@@ -13,4 +16,11 @@ const RANDOM_RANGE_NUMBER = {
   maxRangeInclusive : 9,
 }
 
-module.exports = {INPUT_MESSAGE, OUTPUT_MESSAGE, RANDOM_RANGE_NUMBER }
+const RESULT_MESSAGE = {
+  strike : (strike) => `${strike}스트라이크`,
+  strikeAndBall : (strike,ball) => `${ball}볼 ${strike}스트라이크`,
+  ball : (ball) => `${ball}볼`,
+  nothing : `낫싱`
+}
+
+module.exports = {INPUT_MESSAGE, OUTPUT_MESSAGE, RANDOM_RANGE_NUMBER, RESULT_MESSAGE, SUCCESS }
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,9 +1,18 @@
 const {Console} = require("@woowacourse/mission-utils");
-const { OUTPUT_MESSAGE : {gameGuideMessage}} = require("../Constant");
+const { OUTPUT_MESSAGE : {gameStartGuideMessage, gameOverGuideMessage}, RESULT_MESSAGE : {strike, strikeAndBall, ball , nothing}} = require("../Constant");
 
 const OutputView = {
   printGameStartMessage() {
-    Console.print(gameGuideMessage)
+    Console.print(gameStartGuideMessage)
+  },
+
+  printMap(strikeCount,ballCount) {
+    if(strikeCount === 3 ) Console.print(gameOverGuideMessage)
+    if(strikeCount < 3 && ballCount === 0) Console.print(strike(strikeCount))
+    if(strikeCount < 3 && ballCount > 0) Console.print(strikeAndBall(strikeCount,ballCount))
+    if(strikeCount === 3 && ballCount > 0) Console.print(ball(ballCount))
+    if(strikeCount === 0 && ballCount === 0) Console.print(nothing)
+    
   }
 }
 
