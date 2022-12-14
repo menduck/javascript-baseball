71451a68c68d2d5baa1b20b4b078b77286d7e37c
ming0830 <minsun0830@naver.com>
2022-12-15 04:17:56 +0900

    feat : 입력값 기능 구현 ( 사용자에게 3자리수 입력 받는 메서드, 사용자에게 옵션값 받는 메서드)

---
+++ src/Constant.js
@@ -0,0 +1,11 @@
+const INPUT_MESSAGE = {
+  inputThreeNumberMessage : "숫자를 입력해주세요 : ",
+  optionNumberMessage : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+}
+
+const OUTPUT_MESSAGE = {
+  gameGuideMessage : "숫자 야구 게임을 시작합니다."
+
+}
+
+module.exports = {INPUT_MESSAGE, OUTPUT_MESSAGE }
---
+++ src/View/InputView.js
@@ -0,0 +1,18 @@
+const {Console} = require("@woowacourse/mission-utils");
+const { INPUT_MESSAGE : {inputThreeNumberMessage, optionNumberMessage}} = require("../Constant");
+
+const InputView = {
+  readUserNumbers(userThreeNumbers) {
+    Console.readLine(inputThreeNumberMessage,(numbers) => {
+      userThreeNumbers(numbers.split(',').map((v) => parseInt(v,10)))
+    })
+  },
+
+  readOptionNumber(optionNumber) {
+    Console.readLine(optionNumberMessage, (number) => {
+      optionNumber(parseInt(number,10))
+    })
+  }
+}
+
+module.exports = InputView
