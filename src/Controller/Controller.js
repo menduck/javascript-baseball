b615a54b8b3fc2eb5b4c21862a044aef89c27143
ming0830 <minsun0830@naver.com>
2022-12-15 05:36:39 +0900

    feat : 게임 종료 시 재시작하면 gameStart로 종료시 게임 종료하는 기능 구현

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -3,24 +3,28 @@ const OutputView = require("../View/OutputView")
 const { Console } = require("@woowacourse/mission-utils");
 const InputView = require("../View/InputView");
 const BaseballGame = require("../Model/BaseballGame");
+const { SUCCESS, RETRY_OR_GAMEOVER } = require("../Constant");
 
 
 class Controller {
   #answer
-
   constructor() {
     this.baseballGame = new BaseballGame;
   }
 
-  gameStart() {
+  printgameGuide() {
     OutputView.printGameStartMessage()
-    this.getGameAnswer()
-    Console.print(this.#answer)
-    this.askUserThreeNumbers()
+    this.gameStart()
+  }
+
+  gameStart() {
+      this.askUserThreeNumbers()
+      this.getGameAnswer()
+      Console.print(this.#answer)
   }
 
   getGameAnswer() {
-    this.#answer = baseballMaker()
+    this.#answer = baseballMaker();
   }
 
   askUserThreeNumbers() {
@@ -28,11 +32,32 @@ class Controller {
   }
 
   getHintCount(userThreeNumbers) {
-    const [strikeCount,ballCount] = 
+    [this.strikeCount,this.ballCount] = 
     [this.baseballGame.matchStrike(userThreeNumbers,this.#answer),
-    this.baseballGame.matchBall(userThreeNumbers,this.#answer)]
+    this.baseballGame.matchBall(userThreeNumbers,this.#answer)];
+  
+    this.printResult()
+  }
+  printResult() {
+    OutputView.printMap(this.strikeCount,this.ballCount)
+    this.isSuccess()
+  }
 
-    // Console.print([strikeCount,ballCount])
+  isSuccess() {
+    this.strikeCount === SUCCESS ?  this.askOptionNumber() :this.askUserThreeNumbers()
+  }
+
+  askOptionNumber() {
+    InputView.readOptionNumber(this.retryOrGameOver.bind(this))
+  }
+
+  retryOrGameOver(option) {
+    if(option === RETRY_OR_GAMEOVER.retry ) this.gameStart()
+    this.gameOver()
+  }
+
+  gameOver() {
+    Console.close()
   }
 
 }
