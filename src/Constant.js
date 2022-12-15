5bd12c4e120e3051fe03a53e26bbd34326fb6931
ming0830 <minsun0830@naver.com>
2022-12-15 14:48:05 +0900

    feat : 랜덤 숫자가 4 이상인 자동차 유저의 이름의 배열 반환하는 기능 구현

--- src/Constant.js
+++ src/Constant.js
@@ -1,6 +1,15 @@
+const CAR_GO_RANGE_INCLUSIVE = 4;
+
+const RANDOM_RANGE_NUMBER = {
+  minRangeInclusive : 1,
+  maxRangeInclusive : 9,
+}
 const INPUT_MESSAGE = {
   inputCarNameMessage : '자동차 이름을 5자 이하 콤마로 구분하여 입력해주세요.\n',
   inputTrialMessage : '시도할 횟수를 입력해주세요.\n'
 }
 
-module.exports = {INPUT_MESSAGE};
+const OUTPUT_MESSAGE = {
+  outputGameStartMessage : '자동차 경주 게임을 시작합니다.\n'
+}
+module.exports = {CAR_GO_RANGE_INCLUSIVE,RANDOM_RANGE_NUMBER, INPUT_MESSAGE, OUTPUT_MESSAGE};
--- src/Model/Car.js
+++ src/Model/Car.js
@@ -1,12 +1,23 @@
-const {generate} = require("../utils/RandomGenerator");
+const { CAR_GO_RANGE_INCLUSIVE } = require("../Constant");
+
 
 
 class Car {
-  
+  constructor() {
+
+  }
+
   setUsersRandomNumber(carUsers,generator){
     return carUsers.reduce((acc,curr) => {
       return {...acc, [curr]: generator() };
     }, new Object)
+    
+  }
+
+  moveingUser(randomNumberOfUsers)  {
+    const keysOfUsers = Object.keys(randomNumberOfUsers);
+    return keysOfUsers.filter((number) => randomNumberOfUsers[number] >= CAR_GO_RANGE_INCLUSIVE )
+
   }
 }
 
