165f15e3bd92faea20ae06760d72c62a0cb894b3
ming0830 <minsun0830@naver.com>
2022-12-15 11:52:55 +0900

    refactor : 상수 처리

--- src/Model/BaseballMaker.js
+++ src/Model/BaseballMaker.js
@@ -1,12 +1,12 @@
 const { Random } = require("@woowacourse/mission-utils");
 const {
-  RANDOM_RANGE_NUMBER: { minRangeInclusive, maxRangeInclusive },
+  RANDOM_RANGE_NUMBER: { minRangeInclusive, maxRangeInclusive, numberSize },
 } = require("../Constant");
 
 const BaseballMaker = {
   baseballMaker() {
     let computerNumbers = [];
-    while (computerNumbers.length < 3) {
+    while (computerNumbers.length < numberSize) {
       const randomNumber = Random.pickNumberInRange(
         minRangeInclusive,
         maxRangeInclusive
