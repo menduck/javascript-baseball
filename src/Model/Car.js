640bbf6256307e9f7b3c3115ececdf650bc1463e
ming0830 <minsun0830@naver.com>
2022-12-15 16:15:19 +0900

    fix : 매개인자 없애고 변수 선언

--- src/Model/Car.js
+++ src/Model/Car.js
@@ -4,19 +4,18 @@ const { CAR_GO_RANGE_INCLUSIVE } = require("../Constant");
 
 class Car {
   constructor() {
-
+    this.randomNumberOfUsers = {};
   }
 
   setUsersRandomNumber(carUsers,generator){
-    return carUsers.reduce((acc,curr) => {
+    this.randomNumberOfUsers = carUsers.reduce((acc,curr) => {
       return {...acc, [curr]: generator() };
     }, new Object)
-    
   }
 
-  moveingUser(randomNumberOfUsers)  {
-    const keysOfUsers = Object.keys(randomNumberOfUsers);
-    return keysOfUsers.filter((number) => randomNumberOfUsers[number] >= CAR_GO_RANGE_INCLUSIVE )
+  moveingUser()  {
+    const keysOfUsers = Object.keys(this.randomNumberOfUsers);
+    return keysOfUsers.filter((number) => this.randomNumberOfUsers[number] >= CAR_GO_RANGE_INCLUSIVE )
 
   }
 }
