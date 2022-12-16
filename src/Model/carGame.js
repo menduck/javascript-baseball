913a14e6d7c530da1b48245fa3b6d5139799d609
ming0830 <minsun0830@naver.com>
2022-12-17 02:41:10 +0900

    feat : 자동차 유저네임을 키값으로, 전진하는 횟수를 카운팅하는 것을 값으로 둔 객체를 반환하는 메서드 구현

--- src/Model/carGame.js
+++ src/Model/carGame.js
@@ -1,15 +1,24 @@
 class CarGame {
+  #userGoCount
   constructor() {
-    this.userGoCount = {}
+    this.carNames = {};
+    this.#userGoCount = {};
   }
   carInfo(userName) {
-    this.carNames = Object.values(userName); // {'a','b','c'}
-    this.userGoCount = this.carNames.map((user) => this.userGoCount[user] = 0)
+    this.carNames = Object.values(userName); 
+    this.carNames.map((user) => this.#userGoCount[user] = 0)
   }
+
   randomNumberOfcarUser(userName, generate){
-    userName.reduce((acc,curr) => {
+    this.randomNumberOfUsers = userName.reduce((acc,curr) => {
       return { ...acc, [curr] : generate()}
     },{})
+  }
+
+  move() {
+    for(let key in this.randomNumberOfUsers) {
+      if(this.randomNumberOfUsers[key] >=4) this.#userGoCount[key]++
+    } return this.#userGoCount
   }
 }
 
