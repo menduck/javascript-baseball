7cecd7cfa86f8301a2cf4c03a8356f630ffe7a2a
ming0830 <minsun0830@naver.com>
2022-12-17 02:19:40 +0900

    feat : 유저와 유저의 전진횟수 객체 나눠서 생성

--- src/Model/carGame.js
+++ src/Model/carGame.js
@@ -1,6 +1,10 @@
 class CarGame {
   constructor() {
-    
+    this.userGoCount = {}
+  }
+  carInfo(userName) {
+    this.carNames = Object.values(userName); // {'a','b','c'}
+    this.userGoCount = this.carNames.map((user) => this.userGoCount[user] = 0)
   }
   randomNumberOfcarUser(userName, generate){
     userName.reduce((acc,curr) => {
