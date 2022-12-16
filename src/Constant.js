5060083a603ff754b03e3f642316bfa3b062527f
ming0830 <minsun0830@naver.com>
2022-12-17 03:25:01 +0900

    feat : 사용자 유효성 검사 기능 구현

--- src/Constant.js
+++ src/Constant.js
@@ -17,7 +17,14 @@ const OUTPUT_MESSAGE = {
 
 }
 
-const ERROR_MESSAGE = {}
+const ERROR_MESSAGE = {
+  notFiveStr : '[ERROR]: 자동차 이름은 5글자 이하로 입력하세요.',
+  notBlank : '[ERROR]: 자동자 이름을 지정해주세요.',
+  notAlone : '[ERROR]: 자동차 한 대 이상 입력하세요.',
+  notUnique : '[ERROR]: 중복되지 않은 자동차 이름을 입력하세요.',
+  notNumber : '[ERROR]: 숫자를 입력하세요.',
+
+}
 
 
 
---
+++ src/Validation.js
@@ -0,0 +1,35 @@
+const {Console} = require("@woowacourse/mission-utils");
+const { ERROR_MESSAGE } = require("./Constant");
+const Validation = {
+
+  isVaildCarName(carNameArr) {
+    carNameArr.map((carName) => {
+      if(carName.length > 5 ) {
+        throw Console.print(ERROR_MESSAGE.notFiveStr);
+      }
+
+      if(carName.length === 0 ) {
+        throw Console.print(ERROR_MESSAGE.notBlank)
+      }
+    })
+
+    if(carNameArr.length === 1) {
+      throw Console.print(ERROR_MESSAGE.notAlone); 
+    } 
+
+    const set = new Set(carNameArr);
+    if(set.size !== carNameArr.length){
+      throw Console.print(ERROR_MESSAGE.notUnique)
+    }
+  },
+
+  isValidTrialNumber(trialNumber) {
+    const ONLY_NUMBER_PATTERN = /^[0-9]+$/; // 숫자만
+    if(!ONLY_NUMBER_PATTERN.test(trialNumber)) {
+      throw Console.print(ERROR_MESSAGE.notNumber)
+    }
+  }
+  
+};
+
+module.exports = Validation;
